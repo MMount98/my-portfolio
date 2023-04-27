dda7568b9e79252947db0ada8b2a69ddf4240048
Michael Mount <mount.photos@gmail.com>
2023-04-27 16:01:15 -0600

    built out about me page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
+import AboutMe from "./components/pages/AboutMe";
+
 function App() {
   return (
     <>
       <Router>
         <NavBar />
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<AboutMe />} />
+        </Routes>
       </Router>
     </>
   );
--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -2,22 +2,33 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "3xl": "0 35px 60px -15px rgba(255, 255, 255)",
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [
-      "light",
-      "dark",
-      "bumblebee",
-      "synthwave",
-      "retro",
-      "cyberpunk",
-      "valentine",
-      "garden",
-      "pastel",
-      "luxury",
-      "autumn",
-    ],
+    themes: ["light", "black", "retro", "cyberpunk", "valentine", "garden"],
+    mytheme: {
+      primary: "#14532d",
+
+      secondary: "#78350f",
+
+      accent: "#78350f",
+
+      neutral: "#1c1917",
+
+      "base-100": "#064e3b",
+
+      info: "#38bdf8",
+
+      success: "#38bdf8",
+
+      warning: "#EFD7BB",
+
+      error: "#f3f4f6",
+    },
   },
 };
