0865eeeaf3916a090e1f816691211f7a3b672f00
Michael Mount <mount.photos@gmail.com>
2023-04-30 10:32:41 -0600

    Re-working home page to include framer-motion

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -8,19 +8,19 @@ export default function Navbar() {
   const location = useLocation();
 
   return (
-    <nav className="bg-neutral">
+    <nav className="bg-transparent">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <Link to="/" className="flex-shrink-0">
               <img className="h-12 w-12 rounded-full" src={logo} alt="Logo" />
             </Link>
-            <div className="text-2xl m-4 text-white">Michael Mount</div>
+            <div className="text-2xl m-4">Michael Mount</div>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline">
                 <Link
                   to=""
-                  className={`px-3 py-2 rounded-md text-sm font-medium text-white ${
+                  className={`px-3 py-2 rounded-md text-sm font-medium ${
                     location.pathname === "/"
                       ? "glass"
                       : "hover:glass hover:bg-opacity-75"
@@ -30,7 +30,7 @@ export default function Navbar() {
                 </Link>
                 <Link
                   to="/portfolio"
-                  className={`ml-4 px-3 py-2 rounded-md text-sm font-medium text-white ${
+                  className={`ml-4 px-3 py-2 rounded-md text-sm font-medium  ${
                     location.pathname === "/portfolio"
                       ? "glass"
                       : "hover:glass hover:bg-opacity-75"
@@ -40,7 +40,7 @@ export default function Navbar() {
                 </Link>
                 <Link
                   to="/contact"
-                  className={`ml-4 px-3 py-2 rounded-md text-sm font-medium text-white ${
+                  className={`ml-4 px-3 py-2 rounded-md text-sm font-medium  ${
                     location.pathname === "/contact"
                       ? "glass"
                       : "hover:glass hover:bg-opacity-75"
@@ -50,7 +50,7 @@ export default function Navbar() {
                 </Link>
                 <Link
                   to="/resume"
-                  className={`ml-4 px-3 py-2 rounded-md text-sm font-medium text-white ${
+                  className={`ml-4 px-3 py-2 rounded-md text-sm font-medium  ${
                     location.pathname === "/resume"
                       ? "glass"
                       : "hover:glass hover:bg-opacity-75"
@@ -64,7 +64,7 @@ export default function Navbar() {
           <div className="-mr-2 flex md:hidden">
             <button
               type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-primary focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              className="inline-flex items-center justify-center p-2 rounded-md  hover:bg-primary focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
               onClick={() => setIsOpen(!isOpen)}
@@ -81,31 +81,31 @@ export default function Navbar() {
       </div>
 
       <div
-        className={`${isOpen ? "block" : "hidden"} md:hidden bg-neutral`}
+        className={`${isOpen ? "block" : "hidden"} md:hidden bg-transparent`}
         id="mobile-menu"
       >
         <div className="px-2 pt-2 pb-3 space-y-1">
           <Link
             to=""
-            className="block px-3 py-2 rounded-md text-base font-medium text-white hover:bg-primary hover:bg-opacity-75"
+            className="block px-3 py-2 rounded-md text-base font-medium  hover:bg-primary hover:bg-opacity-75"
           >
             About Me
           </Link>
           <Link
             to=""
-            className="block px-3 py-2 rounded-md text-base font-medium text-white hover:bg-primary hover:bg-opacity-75"
+            className="block px-3 py-2 rounded-md text-base font-medium  hover:bg-primary hover:bg-opacity-75"
           >
             Portfolio
           </Link>
           <Link
             to=""
-            className="block px-3 py-2 rounded-md text-base font-medium text-white hover:bg-primary hover:bg-opacity-75"
+            className="block px-3 py-2 rounded-md text-base font-medium  hover:bg-primary hover:bg-opacity-75"
           >
             Contact Me
           </Link>
           <Link
             to=""
-            className="block px-3 py-2 rounded-md text-base font-medium text-white hover:bg-primary hover:bg-opacity-75"
+            className="block px-3 py-2 rounded-md text-base font-medium  hover:bg-primary hover:bg-opacity-75"
           >
             Resume
           </Link>
--- my-app/src/components/pages/AboutMe.js
+++ my-app/src/components/pages/AboutMe.js
@@ -1,6 +1,6 @@
 import { TypeAnimation } from "react-type-animation";
 import { useEffect } from "react";
-import AboutPhoto from "../images/AboutMe.jpg";
+import AboutPhoto from "../images/IMG_1448.png";
 
 export default function AboutMe() {
   useEffect(() => {
@@ -8,57 +8,36 @@ export default function AboutMe() {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-8">
-      <div className="relative">
-        <img
-          className="w-full h-5/6 object-cover rounded-md "
-          src={AboutPhoto}
-          alt="Michael Mount Headshot"
-          style={{
-            boxShadow: "5px 5px 0.5px rgba(255,255,255, 0.5)"
-          }}
-        />
-      </div>
-      <div>
-        
-          <div className="relative ml-5">
-            <h1 className="text-5xl font-bold">I am</h1>
-            <TypeAnimation
-              className="text-5xl font-bold"
-              sequence={[
-                "a Full-Stack Web Developer",
-                2000,
-                "a Back-End Developer",
-                2000,
-                "a JS Developer",
-                2000,
-                "a Photographer",
-                2000,
-                "an Actor",
-                2000,
-              ]}
-              repeat={Infinity}
-            />
-          </div>
-       
-        <div className="flex flex-col justify-center">
-          <h1 className="text-5xl font-bold my-8 text-center">About Me</h1>
-          <p className="text-xl leading-8 mb-8">
-            Hello, my name is Michael Mount! I am a current Denver University
-            student enrolled in their full-stack flex program. It is a
-            13-week-long course that covers what it means to be a full-stack
-            engineer in the industry. I started my coding journey 2 months prior
-            to starting the program. I have been self-teaching with a focus on
-            HTML, CSS, and Javascript. Prior to joining DU, I attended the
-            University of Northern Colorado and completed my bachelor's degree
-            in Performing Arts and Theatre Studies. I was on the Dean's List for
-            3 years and graduated with an overall GPA of 3.8. Once I finish with
-            my courses, I intend to join the web development industry with high
-            hopes to start a new career in life. Hoping to join a team and work
-            with fellow developers in building a new project!
-          </p>
+    <>
+      <div className="grid grid-cols-1 place-items-center m-6">
+        <div className="relative ml-5 self-start">
+          <TypeAnimation
+            className="text-5xl font-bold"
+            sequence={[
+              "Full-Stack Web Developer",
+              2000,
+              "Back-End Developer",
+              2000,
+              "JS Developer",
+              2000,
+              "Photographer",
+              2000,
+              "Actor",
+              2000,
+            ]}
+            repeat={Infinity}
+          />
         </div>
       </div>
-    </div>
+      <div className="grid grid-cols-1 place-items-center m-14">
+        <div className="relative">
+          <img
+            className="w-96 h-96 rounded-full shadow-lg shadow-white"
+            src={AboutPhoto}
+            alt="Michael Mount Headshot"
+          />
+        </div>
+      </div>
+    </>
   );
 }
