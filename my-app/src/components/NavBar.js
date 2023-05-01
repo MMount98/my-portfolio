a67cd3bdb76a355f5176e40a764812e22a802840
Michael Mount <mount.photos@gmail.com>
2023-05-01 16:29:16 -0600

    fixed navbat moblie design

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -10,12 +10,12 @@ export default function Navbar() {
   return (
     <nav className="bg-transparent">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center justify-around h-16">
           <div className="flex items-center">
             <Link to="/" className="flex-shrink-0">
               <img className="h-12 w-12 rounded-full" src={logo} alt="Logo" />
             </Link>
-            <div className="text-2xl m-4">Michael Mount</div>
+            <div className=" text-xl md:text-2xl m-4">Michael Mount</div>
             <div className="hidden md:block">
               <div className="ml-96 flex">
                 <Link
@@ -86,25 +86,25 @@ export default function Navbar() {
       >
         <div className="px-2 pt-2 pb-3 space-y-1">
           <Link
-            to=""
+            to="/aboutMe"
             className="block px-3 py-2 rounded-md text-base font-medium  hover:bg-primary hover:bg-opacity-75"
           >
             About Me
           </Link>
           <Link
-            to=""
+            to="/portfolio"
             className="block px-3 py-2 rounded-md text-base font-medium  hover:bg-primary hover:bg-opacity-75"
           >
             Portfolio
           </Link>
           <Link
-            to=""
+            to="/contact"
             className="block px-3 py-2 rounded-md text-base font-medium  hover:bg-primary hover:bg-opacity-75"
           >
             Contact Me
           </Link>
           <Link
-            to=""
+            to="/resume"
             className="block px-3 py-2 rounded-md text-base font-medium  hover:bg-primary hover:bg-opacity-75"
           >
             Resume
